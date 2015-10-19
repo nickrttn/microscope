a3d93ba5d5f2241dc9d64fa631f6b7534ba75c2e
Nick Rutten <hello@nickrttn.me>
2015-10-19 11:05:00 +0200

    demonstrate the order that posts appear using a sleep

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,15 +8,13 @@ Template.postSubmit.events({
 		};
 
 		Meteor.call('postInsert', post, function(error, result) {
-			if(error) {
+			if(error)
 				return alert(error.reason);
-			}
 
-			if(result.postExists) {
+			if(result.postExists) 
 				alert('This link has already been posted');
-			}
 
-			Router.go('postPage', {_id: result._id});
+			Router.go('postsList');
 		});
 	}
 });
