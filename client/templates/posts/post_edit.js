7096bc0b3cf01c96d998743d8063699fadd66de2
Nick Rutten <hello@nickrttn.me>
2015-10-20 23:14:05 +0200

    validate post contents when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,16 @@
+Template.postEdit.onCreated(function() {
+	Session.set('postEditErrors', {});
+});
+
+Tempalte.postEdit.helpers({
+	errorMessage: function(field) {
+		return Session.get('postEditErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+	}
+});
+
 Template.postEdit.events({
 	'submit form': function(e) {
 		e.preventDefault();
@@ -7,6 +20,11 @@ Template.postEdit.events({
 		var postProperties = {
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
+		}
+
+		var errors = validatePost(postProperties);
+		if (errors.title || errors.url) {
+			return Session.set('postEditErrors', errors);
 		}
 
 		Posts.update(currentPostId, {$set: postProperties}, function(error) {
@@ -21,7 +39,7 @@ Template.postEdit.events({
 	'click .delete': function(e) {
 		e.preventDefault();
 
-		if(confirm('Delete this post?')) {
+		if (confirm('Delete this post?')) {
 			var currentPostId = this._id;
 			Posts.remove(currentPostId);
 			Router.go('postsList');
