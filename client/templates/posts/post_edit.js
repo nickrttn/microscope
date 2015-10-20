3281f8b79c65a8138599f9117455379c021c814e
Nick Rutten <hello@nickrttn.me>
2015-10-20 22:13:42 +0200

    added redundant link check to postEdit

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -5,13 +5,15 @@ Template.postEdit.events({
 		var currentPostId = this._id;
 
 		var postProperties = {
-			url: $(e.target).find('[name=url').val(),
+			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		}
 
 		Posts.update(currentPostId, {$set: postProperties}, function(error) {
 			if (error) {
 				alert(error.reason);
+			} else if (Posts.findOne({url: postProperties.url})){
+				alert('There\'s already a post with that URL.');
 			} else {
 				Router.go('postPage', {_id: currentPostId});
 			}
